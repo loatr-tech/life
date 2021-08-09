d9d0505e6fdd76fafcc3e971d4531df2dcfafaef
Shawn Chen <ethanloatrchen@gmail.com>
2021-08-08 18:57:49 -0700

    Update favicon

--- src/posts/post-mian.tsx
+++ src/posts/post-mian.tsx
@@ -4,34 +4,6 @@ import Editor from 'rich-markdown-editor';
 import './post-main.scss';
 import PostComments from './post-comments';
 
-const exampleText = `
-haha
-# H1
-## H2
-### H3
-# 讲道理就是写着玩的\n\n我是认真说的
-Dave wasn't exactly sure how he had ended up in this predicament. He ran
-through all the events that had lead to this current situation and it
-still didn't make sense. He wanted to spend some time to try and make
-sense of it all, but he had higher priorities at the moment. The first
-was how to get out of his current situation of being naked in a tree
-with snow falling all around and no way for him to get down.
-
-Dave wasn't exactly sure how he had ended up in this predicament. He ran
-through all the events that had lead to this current situation and it
-still didn't make sense. He wanted to spend some time to try and make
-sense of it all, but he had higher priorities at the moment. The first
-was how to get out of his current situation of being naked in a tree
-with snow falling all around and no way for him to get down.
-
-Dave wasn't exactly sure how he had ended up in this predicament. He ran
-through all the events that had lead to this current situation and it
-still didn't make sense. He wanted to spend some time to try and make
-sense of it all, but he had higher priorities at the moment. The first
-was how to get out of his current situation of being naked in a tree
-with snow falling all around and no way for him to get down.
-`;
-
 function PostMain({ post }: any) {
   return (
     <div className="post-main">

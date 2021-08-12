97d99b04af54d6d5cb160d61dda23e1e24302d90
Shawn Chen <ethanloatrchen@gmail.com>
2021-08-12 09:08:17 -0700

    Comments section

--- src/posts/post-comments.tsx
+++ src/posts/post-comments.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Comment, Avatar } from 'antd';
+import { Comment, Avatar, Divider, Button } from 'antd';
 import './post-comments.scss';
 
 const ExampleComment = ({ children }: any) => (
   <Comment
-    actions={[<span key="comment-nested-reply-to">Reply to</span>]}
-    author={<span>Han Solo</span>}
-    avatar={
-      <Avatar
-        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-        alt="Han Solo"
-      />
-    }
+    actions={[
+      <span key="comment-nested-reply-to">
+        <i className="fas fa-reply"></i> 回复
+      </span>,
+      <span key="comment-nested-reply-to">
+        <i className="fas fa-quote-left"></i> 引用
+      </span>,
+    ]}
+    author={<span>岸上某位用户</span>}
+    avatar={<Avatar>A</Avatar>}
     content={
       <p>
         We supply a series of design principles, practical patterns and high
@@ -26,14 +28,47 @@ const ExampleComment = ({ children }: any) => (
 function PostComments() {
   return (
     <section className="post-comments">
-      <ExampleComment>
-        <ExampleComment />
-        <ExampleComment />
-      </ExampleComment>
-      <ExampleComment>
-        <ExampleComment />
-        <ExampleComment />
-      </ExampleComment>
+      <h3>
+        <i className="far fa-comments"></i> 评论区 (24)
+      </h3>
+      <div className="post-comments__thread">
+        <div className="post-comments__thread-head">
+          <ExampleComment />
+        </div>
+        <div className="post-comments__thread-replys">
+          <ExampleComment />
+          <ExampleComment />
+          <ExampleComment />
+          <div className="post-comments__thread-expand">展开更多回复 (12)</div>
+        </div>
+      </div>
+      <div className="post-comments__thread">
+        <div className="post-comments__thread-head">
+          <ExampleComment />
+        </div>
+      </div>
+      <div className="post-comments__thread">
+        <div className="post-comments__thread-head">
+          <ExampleComment />
+        </div>
+        <div className="post-comments__thread-replys">
+          <ExampleComment />
+        </div>
+      </div>
+      <Divider />
+      <div className="post-comments__textarea-container">
+        <span className="post-comments__textarea-avatar">
+          <Avatar size={36}>M</Avatar>
+        </span>
+        <textarea
+          className="post-comments__textarea"
+          name="comment-textarea"
+          rows={4}
+        ></textarea>
+      </div>
+      <div className="post-comments__textarea-actions">
+        <Button>留言</Button>
+      </div>
     </section>
   );
 }

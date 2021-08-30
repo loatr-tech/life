647d5ae2134aafb2a2a974601afe89b92ea8e21c
Shawn Chen <ethanloatrchen@gmail.com>
2021-08-30 11:00:42 -0700

    Adjust category

--- src/_utils/categories.ts
+++ src/_utils/categories.ts
@@ -1,3 +1,78 @@
+const TOP_CATEGORY_TEXT = {
+  CAREER: '打工人儿',
+  GO_ABROAD: '留学移民',
+  STUDY: '天天向上',
+  NEIGHBORHOOD: '街坊领居',
+};
+
+export const CATEGORY = {
+  // 打工人儿
+  CAREER: {
+    EXP_SHARING: 'career_exp_sharing',
+    PROMO: 'career_promo',
+    TC: 'total_package',
+    INTERVIEW_QUESTIONS: 'interview_questions',
+    NEED_REFERRAL: 'need_referral',
+    WE_RE_HIRING: 'we_re_hiring',
+  },
+  // 留学签证
+  APPLY_SCHOOL: 'apply_school',
+  APPLY_SCHOOL_RESULTS: 'apply_school_results',
+  WORK_VISA: 'work_visa',
+  GREEN_CARD: 'green_card',
+  STUDENT_VISA: 'student_visa',
+  // 好好学习
+  LETS_STUDY: 'lets_study',
+  LEET_CODE: 'leet_code',
+  // 街坊邻居
+  USED_ITEMS: 'used_items',
+  HOUSE_RENTAL: 'house_rental',
+};
+
+export const CATEGORIES_MAP: any = {
+  // 打工人儿
+  [CATEGORY.CAREER.EXP_SHARING]: '职场经验',
+  [CATEGORY.CAREER.PROMO]: '升职加薪',
+  [CATEGORY.CAREER.TC]: '工资包裹',
+  [CATEGORY.CAREER.INTERVIEW_QUESTIONS]: '面试经验',
+  [CATEGORY.CAREER.NEED_REFERRAL]: '跪求内推',
+  [CATEGORY.CAREER.WE_RE_HIRING]: '我要招人',
+  // 留学签证
+  [CATEGORY.APPLY_SCHOOL]: '申请学校',
+  [CATEGORY.APPLY_SCHOOL_RESULTS]: '申校结果',
+  [CATEGORY.WORK_VISA]: '工作签证',
+  [CATEGORY.GREEN_CARD]: '移民绿卡',
+  [CATEGORY.STUDENT_VISA]: '学生签证',
+  // 好好学习
+  [CATEGORY.LETS_STUDY]: '我要自习',
+  [CATEGORY.LEET_CODE]: '组队刷题',
+  // 街坊邻居
+  [CATEGORY.USED_ITEMS]: '二手市场',
+  [CATEGORY.HOUSE_RENTAL]: '房屋出租',
+};
+
+export const CATEGORIES_PARENT: any = {
+  // 打工人儿
+  [CATEGORY.CAREER.EXP_SHARING]: TOP_CATEGORY_TEXT.CAREER,
+  [CATEGORY.CAREER.PROMO]: TOP_CATEGORY_TEXT.CAREER,
+  [CATEGORY.CAREER.TC]: TOP_CATEGORY_TEXT.CAREER,
+  [CATEGORY.CAREER.INTERVIEW_QUESTIONS]: TOP_CATEGORY_TEXT.CAREER,
+  [CATEGORY.CAREER.NEED_REFERRAL]: TOP_CATEGORY_TEXT.CAREER,
+  [CATEGORY.CAREER.WE_RE_HIRING]: TOP_CATEGORY_TEXT.CAREER,
+  // 留学移民
+  [CATEGORY.APPLY_SCHOOL]: TOP_CATEGORY_TEXT.GO_ABROAD,
+  [CATEGORY.APPLY_SCHOOL_RESULTS]: TOP_CATEGORY_TEXT.GO_ABROAD,
+  [CATEGORY.WORK_VISA]: TOP_CATEGORY_TEXT.GO_ABROAD,
+  [CATEGORY.GREEN_CARD]: TOP_CATEGORY_TEXT.GO_ABROAD,
+  [CATEGORY.STUDENT_VISA]: TOP_CATEGORY_TEXT.GO_ABROAD,
+  // 天天向上
+  [CATEGORY.LETS_STUDY]: TOP_CATEGORY_TEXT.STUDY,
+  [CATEGORY.LEET_CODE]: TOP_CATEGORY_TEXT.STUDY,
+  // 街坊邻居
+  [CATEGORY.USED_ITEMS]: TOP_CATEGORY_TEXT.NEIGHBORHOOD,
+  [CATEGORY.HOUSE_RENTAL]: TOP_CATEGORY_TEXT.NEIGHBORHOOD,
+};
+
 export const CATEGORIES = [
   {
     id: 'career',
@@ -10,10 +85,10 @@ export const CATEGORIES = [
         name: '经验分享',
         subcategories: [
           {
-            id: 'experience_sharing',
+            id: CATEGORY.CAREER.EXP_SHARING,
           },
           {
-            id: 'promotion',
+            id: CATEGORY.CAREER.PROMO,
           },
         ],
       },
@@ -23,10 +98,10 @@ export const CATEGORIES = [
         name: '数据分享',
         subcategories: [
           {
-            id: 'total_package',
+            id: CATEGORY.CAREER.TC,
           },
           {
-            id: 'interview_questions',
+            id: CATEGORY.CAREER.INTERVIEW_QUESTIONS,
           },
         ],
       },
@@ -36,10 +111,10 @@ export const CATEGORIES = [
         name: '职位分享',
         subcategories: [
           {
-            id: 'need_referral',
+            id: CATEGORY.CAREER.NEED_REFERRAL,
           },
           {
-            id: 'we_re_hiring',
+            id: CATEGORY.CAREER.WE_RE_HIRING,
           },
         ],
       },
@@ -56,10 +131,10 @@ export const CATEGORIES = [
         name: '留学申请',
         subcategories: [
           {
-            id: 'apply_school',
+            id: CATEGORY.APPLY_SCHOOL,
           },
           {
-            id: 'application_results',
+            id: CATEGORY.APPLY_SCHOOL_RESULTS,
           },
         ],
       },
@@ -69,13 +144,13 @@ export const CATEGORIES = [
         name: '移民签证',
         subcategories: [
           {
-            id: 'work_visa',
+            id: CATEGORY.WORK_VISA,
           },
           {
-            id: 'green_card',
+            id: CATEGORY.GREEN_CARD,
           },
           {
-            id: 'student_visa',
+            id: CATEGORY.STUDENT_VISA
           },
         ],
       },
@@ -87,10 +162,10 @@ export const CATEGORIES = [
     icon: 'fas fa-book-reader',
     subcategories: [
       {
-        id: 'lets_study',
+        id: CATEGORY.LETS_STUDY
       },
       {
-        id: 'leet_code',
+        id: CATEGORY.LEET_CODE
       },
     ],
   },
@@ -100,53 +175,11 @@ export const CATEGORIES = [
     icon: 'fas fa-people-arrows',
     subcategories: [
       {
-        id: 'used_items',
+        id: CATEGORY.USED_ITEMS
       },
       {
-        id: 'house_rental',
+        id: CATEGORY.HOUSE_RENTAL
       },
     ],
   },
 ];
-
-export const CATEGORIES_MAP: any = {
-  // 打工人儿
-  experience_sharing: '经验分享',
-  promotion: '升职加薪',
-  total_package: '我的包裹',
-  interview_questions: '北美面经',
-  need_referral: '跪求内推',
-  we_re_hiring: '我要招人',
-  // 留学签证
-  apply_school: '申请学校',
-  application_results: '申校结果',
-  work_visa: '工作签证',
-  green_card: '移民绿卡',
-  student_visa: '学生签证',
-  // 好好学习
-  lets_study: '我要自习',
-  leet_code: '组队刷题',
-  // 街坊邻居
-  used_items: '二手市场',
-  house_rental: '房屋出租',
-};
-
-export const CATEGORIES_PARENT: any = {
-  // 打工人儿
-  experience_sharing: '打工人儿',
-  promotion: '打工人儿',
-  total_package: '打工人儿',
-  interview_questions: '打工人儿',
-  need_referral: '打工人儿',
-  we_re_hiring: '打工人儿',
-  // 留学签证
-  work_visa: '留学移民',
-  green_card: '留学移民',
-  student_visa: '留学移民',
-  // 好好学习
-  lets_study: '天天向上',
-  leet_code: '天天向上',
-  // 街坊邻居
-  used_items: '街坊领居',
-  house_rental: '街坊领居',
-};

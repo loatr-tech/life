a2b6896764bd3530c99a498da0fe10154efef3d6
Shawn Chen <ethanloatrchen@gmail.com>
2023-08-12 11:38:25 -0700

    Adjust post card message

--- web/src/post/post-card.tsx
+++ web/src/post/post-card.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Avatar } from 'antd';
 
 import './post-card.scss';
-import { timeSince } from '../_utils/time';
 import { displayCount } from '../_utils/number';
 import { CATEGORIES_MAP } from '../_utils/categories';
 
@@ -11,42 +10,50 @@ function PostCard({ post }: any) {
   return (
     <Link to={`/post/${post.id}`} className="post-card__details">
       <div className="post-card">
-        <section className="post-card__stats">
-          <Avatar
-            className="post-card__stats-avatar"
-            src={post?.owner?.avatar_url}
-          />
-          <div className="post-card__stats-username">
-            <div style={{ color: 'black' }}>{post?.owner?.name}</div>
-            <div className="post-card__stats-ago">
-              {timeSince(post?.createdAt)}
-            </div>
-          </div>
-          <div className="post-card__stats-icon">
-            <i className="far fa-eye"></i>{' '}
-            {displayCount(post?.interactions?.views)}
-          </div>
-          <div className="post-card__stats-icon">
-            <i className="far fa-thumbs-up"></i>{' '}
-            {displayCount(post?.interactions?.vlikes)}
-          </div>
-          <div className="post-card__stats-icon">
-            <i className="far fa-comment"></i>{' '}
-            {displayCount(post?.interactions?.vcomments)}
-          </div>
-        </section>
-        <hr className="post-card__divider" />
-
-        <div className="post-card__details-title-container">
-          <h3 className="post-card__details-title">{post.title}</h3>
-          <span className="post-card__details-category">
+        {/* Header */}
+        <div className="post-card__header">
+          <h3 className="post-card__title">{post.title}</h3>
+          <span className="post-card__category">
             {CATEGORIES_MAP[post.category] || '未知分类'}
           </span>
         </div>
-        <p className="post-card__details-content">
+
+        {/* Content */}
+        <p className="post-card__content">
           {post.content ||
             'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi nemo mollitia repellendus, laudantium unde, vel iste tempora eum nihil aperiam aliquid? Deleniti cum beatae iste sapiente molestias ullam consequatur alias.'}
         </p>
+
+        <hr className="post-card__divider" />
+
+        {/* Card footer */}
+        <div className="post-card__footer">
+          <section className="post-card__stats">
+            <div className="post-card__stats-icon">
+              <i className="far fa-eye"></i>{' '}
+              {displayCount(post?.interactions?.views)}
+            </div>
+            <div className="post-card__stats-icon">
+              <i className="far fa-thumbs-up"></i>{' '}
+              {displayCount(post?.interactions?.vlikes)}
+            </div>
+            <div className="post-card__stats-icon">
+              <i className="far fa-comment"></i>{' '}
+              {displayCount(post?.interactions?.vcomments)}
+            </div>
+          </section>
+
+          <section className="post-card__author">
+            <Avatar
+              className="post-card__author-avatar"
+              src={post?.owner?.avatar_url}
+              size={16}
+            />
+            <span className="post-card__author-username">
+              {post?.owner?.name}
+            </span>
+          </section>
+        </div>
       </div>
     </Link>
   );
